c4b39ecbb4c834b835cba29dc8b77b157aa6d169
Jan Graichen <jg@altimos.de>
2013-03-07 23:38:06 +0100

    Add tests on ActiveRecord models and associations.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,6 +8,7 @@ ENV["RAILS_ENV"] = "test"
 
 require 'active_support'
 require 'action_controller'
+require 'active_record'
 require 'minitest/reporters'
 MiniTest::Reporters.use!
 
@@ -21,6 +22,31 @@ end
 class ActiveSupport::TestCase
   setup do
     @routes = Responders::Routes
+  end
+end
+
+class ArModel < ActiveRecord::Base
+  has_many :ar_assoc_models
+  def as_json(opts = {}); self.id end
+end
+
+class ArAssocModel < ActiveRecord::Base
+  def as_json(opts = {}); self.id end
+end
+
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :database  => ':memory:'
+)
+ActiveRecord::Base.connection.execute(
+    'CREATE TABLE ar_models (id INTEGER PRIMARY KEY AUTOINCREMENT);')
+ActiveRecord::Base.connection.execute(
+    'CREATE TABLE ar_assoc_models (id INTEGER PRIMARY KEY AUTOINCREMENT, ar_model_id INTEGER);')
+676.times do
+  ArModel.create!.tap do |ar_model|
+    5.times do
+      ar_model.ar_assoc_models.create!
+    end
   end
 end
 
