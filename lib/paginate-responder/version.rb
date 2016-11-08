3b759ecb3aacfce6bf68bdd5c9562cbea689aadc
Jan Graichen <jgraichen@altimos.de>
2016-11-08 15:39:08 +0100

    Release v1.6.0

--- lib/paginate-responder/version.rb
+++ lib/paginate-responder/version.rb
@@ -3,7 +3,7 @@ module PaginateResponder
     MAJOR = 1
     MINOR = 6
     PATCH = 0
-    STAGE = :b0
+    STAGE = nil
 
     def self.to_s
       [MAJOR, MINOR, PATCH, STAGE].reject(&:nil?).join '.'
